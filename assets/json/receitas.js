cc1ee9e62f8bf701c715947690763130cb4f26d9
JPCassimiro <jp.cassimironf@gmail.com>
2024-05-28 09:53:45 -0300

    conteúdo dinâmico

--- assets/json/receitas.js
+++ assets/json/receitas.js
@@ -25,7 +25,7 @@ const listaReceitas = {
                 "Açúcar mascavo - 1 colher de chá",
                 "Salsa picada - ½ xícara de chá",
                 "Cebolinha picada - ½ xícara de",
-                "Sal"
+                "Semen"
             ],
             preparo:"Modo de preparo: Refogue a cebolinha e as cenouras no óleo vegetal, mexendo de vez em quando. Acrescente a água, sal, o açúcar e os inhames. Cubra e cozinhe lentamente por 20 minutos. Passe a sopa na peneira. Volte ao fogo, prove e salpique a salsa.",
             linkImg:"https://receitas123.com/wp-content/uploads/2023/09/sopa-de-inhame-com-cenoura.png"
--- js/conteudo.js
+++ js/conteudo.js
@@ -1,3 +1,33 @@
-const updateConteudo = (conteudo) => {
-    
+console.log(listaReceitas)
+function getParameterByName(name, url) {//encontra receita baseado no url
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+let receitaAtual = getParameterByName('key');//key da receita baseado na lista
+
+const updateConteudo = () => {
+    //let fig = document.getElementById("figureReceita");
+    let h = document.getElementById("hReceita");//pega os elementos a serem modificados
+    let ul = document.getElementById("ulIngradientes");
+    let p = document.getElementById("pPreparo");
+    let img = document.getElementById("imagemConteudo");
+    img.src = listaReceitas.receitas[receitaAtual].linkImg;
+    img.alt = listaReceitas.receitas[receitaAtual].nome;
+    let titulo = document.createTextNode(listaReceitas.receitas[receitaAtual].nome);
+    h.appendChild(titulo);
+    let preparo = document.createTextNode(listaReceitas.receitas[receitaAtual].preparo);
+    p.appendChild(preparo);
+    Object.keys(listaReceitas.receitas[receitaAtual].ingredientes).forEach(key=>{//percorre o array de reicetas dentro do objeto
+        //console.log(listaReceitas.receitas[receitaAtual].ingredientes[key]);
+        let li = document.createElement("li");
+        let ingrediente = document.createTextNode(listaReceitas.receitas[receitaAtual].ingredientes[key]);
+        li.appendChild(ingrediente);
+        ul.appendChild(li);
+    });
 }
