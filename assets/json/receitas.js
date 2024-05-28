25a51440acd54c85da47820aeea28fbd527832e0
JPCassimiro <jp.cassimironf@gmail.com>
2024-05-28 08:52:49 -0300

    lista de receitas dinâmica

---
+++ assets/json/receitas.js
@@ -0,0 +1,34 @@
+const listaReceitas = {
+    receitas:[
+        {
+            nome: "Sopa de cenoura com Inhame",
+            ingredientes:[
+                "Óleo vegetal - 3 colheres de sopa",
+                "Cenoura fatiada - 2 ½ xícaras de chá",
+                "Inhame descascado e picado - 3 xícaras de chá",
+                "Água - 6 xícaras de chá",
+                "Açúcar mascavo - 1 colher de chá",
+                "Salsa picada - ½ xícara de chá",
+                "Cebolinha picada - ½ xícara de",
+                "Sal"
+            ],
+            preparo:"Modo de preparo: Refogue a cebolinha e as cenouras no óleo vegetal, mexendo de vez em quando. Acrescente a água, sal, o açúcar e os inhames. Cubra e cozinhe lentamente por 20 minutos. Passe a sopa na peneira. Volte ao fogo, prove e salpique a salsa.",
+            linkImg:"https://receitas123.com/wp-content/uploads/2023/09/sopa-de-inhame-com-cenoura.png"
+        },
+        {
+            nome: "Sopa de cenoura com Inhame",
+            ingredientes:[
+                "Óleo vegetal - 3 colheres de sopa",
+                "Cenoura fatiada - 2 ½ xícaras de chá",
+                "Inhame descascado e picado - 3 xícaras de chá",
+                "Água - 6 xícaras de chá",
+                "Açúcar mascavo - 1 colher de chá",
+                "Salsa picada - ½ xícara de chá",
+                "Cebolinha picada - ½ xícara de",
+                "Sal"
+            ],
+            preparo:"Modo de preparo: Refogue a cebolinha e as cenouras no óleo vegetal, mexendo de vez em quando. Acrescente a água, sal, o açúcar e os inhames. Cubra e cozinhe lentamente por 20 minutos. Passe a sopa na peneira. Volte ao fogo, prove e salpique a salsa.",
+            linkImg:"https://receitas123.com/wp-content/uploads/2023/09/sopa-de-inhame-com-cenoura.png"
+        }
+    ]
+}
---
+++ js/conteudo.js
@@ -0,0 +1,3 @@
+const updateConteudo = (conteudo) => {
+    
+}
